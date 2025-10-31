ba9b23305fd828d1574aa643f658766a188fbe01
anishkunwar69 <anishkunwar808@gmail.com>
2025-10-31 20:36:11 +0545

    hidden-services-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <Hero />
       <Testimonials />
-      <Services />
+      {/* <Services /> */}
       <WhoWeWorkWith />
       <Portfolio />
       <Faq />
